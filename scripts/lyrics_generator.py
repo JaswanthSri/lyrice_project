2560710a494158cf777933b1f0fd732d548587c3
srivatchangv2006 <gvsrivatchan@gmail.com>
2024-12-04 21:21:33 +0530

    adding a new line

--- scripts/lyrics_generator.py
+++ scripts/lyrics_generator.py
@@ -9,3 +9,5 @@ generated_lyrics = generator(seed_text, max_length=100, num_return_sequences=1)
 
 print("Generated Lyrics:\n", generated_lyrics[0]['generated_text'])
 #testing 
+
+#this is srivatchan testing 
